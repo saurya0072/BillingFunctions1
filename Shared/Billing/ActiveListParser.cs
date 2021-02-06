6427914b248bc93816965269793f74cbfcbb1e26
kumar gaurav <kgaurav.iem@gmail.com>
2021-02-06 15:01:12 +0000

    test

---
+++ Shared/Billing/ActiveListParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Shared.Billing
+{
+    public static class ActiveListParser
+    {
+        public static ActiveList Parse(string name, Stream myBlob)
+        {
+            using (StreamReader sr = new StreamReader(myBlob))
+            {
+                var dataLines = sr.ReadToEnd();
+                var parts = name.Split(new char[] { '_' });
+                return new ActiveList
+                {
+                    CustomerCode = parts[0],
+                    Year = int.Parse(parts[1]),
+                    Month = int.Parse(parts[2]),
+                    DataLines = dataLines.Split(Environment.NewLine.ToCharArray())
+                };
+            }
+        }
+    }
+
+    public class ActiveList
+    {
+        public string CustomerCode { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public string[] DataLines { get; set; }
+    }
+}
